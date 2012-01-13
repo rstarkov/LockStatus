ca8bd203704d270c84f832a2204a34a1697577d8
rstarkov <sourcecode@roman.st>
2012-01-13 19:23:32 +0000

    GPL header. Better-looking icons. Program details.

--- LockStatus.cpp
+++ LockStatus.cpp
@@ -1,3 +1,15 @@
+// Copyright 2012 Roman Starkov
+//
+// LockStatus is free software: you can redistribute it and/or modify it under
+// the terms of the GNU General Public License as published by the Free
+// Software Foundation, either version 3 of the License, or (at your option)
+// any later version.
+//
+// LockStatus is distributed in the hope that it will be useful, but WITHOUT
+// ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+// more details.
+
 #include "stdafx.h"
 
 HINSTANCE hInstance;
@@ -144,7 +156,7 @@ int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nC
         }
     }
 
-    // Grab ourselves a message-only window, which is required by pretty much every piece of functionality this program has
+    // Grab a message-only window, which is required by pretty much every piece of functionality this program has
     hInstance = hInst;
     hMessageWnd = CreateWindow(L"Message", L"Dummy", 0, 0, 0, 0, 0, HWND_MESSAGE, NULL, hInstance, NULL);
 
